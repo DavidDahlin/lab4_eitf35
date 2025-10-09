8545dddea6d3c05aba163a76f6663f797ecddee6
Tvånar <eandr98@gmail.com>
2025-10-09 16:30:51 +0200

    initial constructs

---
+++ src/display/vga_driver_top.sv
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+
+
+module vga_driver_top(
+    // top level signals
+    input logic clk,
+    input logic reset_n,
+
+    // Display inputs
+    input logic[7:0] a,
+    input logic[7:0] b,
+    input logic[7:0] result,
+    input logic[3:0] operand,
+
+    // Control signals
+    input logic valid,
+
+    // Outputs
+    output logic[11:0] io_rgb_color,
+); 
+
+
+    
+endmodule
