f08deea63f80bd631a7b6e4592fc446f0e07b42a
Tvånar <eandr98@gmail.com>
2025-10-31 13:22:01 +0100

    edge hold

--- src/debug/edge_holder.sv
+++ src/debug/edge_holder.sv
@@ -1,18 +1,20 @@
 `timescale 1ns/1ps
 
-module edge_holder(.
+module edge_holder #(
+    parameter int W = 0
+)(
     input logic clk,
     input logic rst,
-    input logic edge_in,
-    output logic hold
+    input logic [W:0] edge_in,
+    output logic [W:0] hold
 );
 
-logic [15:0] c, c_n;
+logic [28:0] c, c_n;
 
-logic e_l, e_ln;
+logic [W:0] e_l, e_ln;
 
 always_ff @(posedge clk or negedge rst) begin
-    if (rst == 1) begin
+    if (rst == 0) begin
         e_l <= '0;
         c <= '0;
     end else begin
@@ -24,15 +26,15 @@ end
 always_comb begin
     c_n = c +1;
     
-    if(edge_in == 1) c_n = 0;
+    if(edge_in == 1) c_n = '0;
 
 end
 
 always_comb begin
     e_ln = e_l;
     
-    if(edge_in == 1) e_ln = 1;
-    else if (c == 16'hffff) e_ln = 0;
+    if(edge_in != '0) e_ln = edge_in;
+    else if (c == 28'hfffffff) e_ln = '0;
 end
 
 assign hold = e_l;
